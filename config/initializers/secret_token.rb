69861ed786497a20c6fab5a4a40b2ea2cb4b51a3
Clive C. Banditi <kreativeq@gmail.com>
2013-02-13 10:00:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knyna::Application.config.secret_token = '51b02efe6e2bc311917ebf96ed72ee3025c31988c8a4f5a0750da66aa26d09bd62afb1f93a0b1d185eaec7439cd44ef2906e49aa72b60bd1663deb75b4b8b6ca'
